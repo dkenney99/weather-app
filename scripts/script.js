c1f91cbed51c608780a0237063be934f7888bc37
Daniel Kenney <danielkenney99@yahoo.com>
2022-11-14 23:39:11 -0800

    full weather app finished

---
+++ scripts/script.js
@@ -0,0 +1,60 @@
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+
+const updateUI = (data) => {
+  const cityDets = data.cityDets;
+  const weather = data.weather;
+
+  //update details template
+  details.innerHTML = `
+  <h5 class="my-3">${cityDets.EnglishName}</h5>
+  <div class="my-3">${weather.WeatherText}</div>
+  <div class="display-4 my-4">
+    <span>${weather.Temperature.Imperial.Value}</span>
+    <span>&deg;F</span>
+  </div>
+  `;
+
+  //update the night and day
+
+  let timeSrc = null;
+  if (weather.IsDayTime) {
+    timeSrc = "/img/day.svg";
+  } else {
+    timeSrc = "/img/night.svg";
+  }
+
+  time.setAttribute("src", timeSrc);
+
+  //remove the d-none class if present
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
+
+const updateCity = async (city) => {
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
+
+  return {
+    cityDets: cityDets,
+    weather: weather,
+  };
+};
+
+cityForm.addEventListener("submit", (e) => {
+  //prevent default
+  e.preventDefault();
+
+  // get city value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  //update UI with the
+  updateCity(city)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
+});
